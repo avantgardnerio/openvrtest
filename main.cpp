c792dd6c840abbfe1ea835e856468daacc014759
Brent Gardner <bgardner@squarelabs.net>
2017-01-24 22:41:26 -0700

    Can spit out pose information

--- main.cpp
+++ main.cpp
@@ -2,28 +2,59 @@
 
 #include "openvr.h"
 
+#include "geom/Matrices.h"
+
+Matrix4 steamMatToMatrix4( const vr::HmdMatrix34_t &matPose ) {
+    Matrix4 matrixObj(
+            matPose.m[0][0], matPose.m[1][0], matPose.m[2][0], 0.0,
+            matPose.m[0][1], matPose.m[1][1], matPose.m[2][1], 0.0,
+            matPose.m[0][2], matPose.m[1][2], matPose.m[2][2], 0.0,
+            matPose.m[0][3], matPose.m[1][3], matPose.m[2][3], 1.0f
+    );
+    return matrixObj;
+}
+
 int main() {
+
+    // Init HMD
     vr::EVRInitError eError = vr::VRInitError_None;
     vr::IVRSystem *hmd = vr::VR_Init(&eError, vr::VRApplication_Scene);
     if (eError != vr::VRInitError_None) {
         printf("Unable to init VR runtime: %s", vr::VR_GetVRInitErrorAsEnglishDescription(eError));
-        return false;
+        return -1;
     }
-    vr::ETrackedPropertyError err;
-    for (uint32_t deviceIdx = vr::k_unTrackedDeviceIndex_Hmd + 1; deviceIdx < vr::k_unMaxTrackedDeviceCount; deviceIdx++)	{
-        if (!hmd->IsTrackedDeviceConnected(deviceIdx))
-            continue;
-        uint32_t buffLen = hmd->GetStringTrackedDeviceProperty(deviceIdx, vr::Prop_TrackingSystemName_String, NULL, 0, &err);
-        if (buffLen == 0) {
-            return false;
-        }
+    if (!vr::VRCompositor()) {
+        printf("Compositor initialization failed. See log file for details\n", __FUNCTION__);
+        return -1;
+    }
 
-        char *buff = new char[buffLen];
-        buffLen = hmd->GetStringTrackedDeviceProperty(deviceIdx, vr::Prop_TrackingSystemName_String, buff, buffLen, &err);
-        std::string sResult = buff;
-        delete[] buff;
-        std::cout << sResult;
+    // Main loop
+    vr::TrackedDevicePose_t devicePose[ vr::k_unMaxTrackedDeviceCount ];
+    Matrix4 devicePoseMat[ vr::k_unMaxTrackedDeviceCount ];
+    Matrix4 leftHandPose;
+    Matrix4 rightHandPose;
+    while(true) {
+        vr::VRCompositor()->WaitGetPoses(devicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0);
+        int hand = 0;
+        for (int deviceIdx = 0; deviceIdx < vr::k_unMaxTrackedDeviceCount; ++deviceIdx) {
+            if (!devicePose[deviceIdx].bPoseIsValid) {
+                continue;
+            }
+            devicePoseMat[deviceIdx] = steamMatToMatrix4(devicePose[deviceIdx].mDeviceToAbsoluteTracking);
+            if (hmd->GetTrackedDeviceClass(deviceIdx) == vr::TrackedDeviceClass_Controller) {
+                if (hand == 0) {
+                    rightHandPose = devicePoseMat[deviceIdx];
+                    std::cout << rightHandPose;
+                }
+                if (hand == 1) {
+                    leftHandPose = devicePoseMat[deviceIdx];
+                    std::cout << leftHandPose;
+                }
+                hand++;
+            }
+        }
     }
 
     return 0;
 }
+
