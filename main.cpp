9d1970ea2692af562bfa09905fd52cfbc26561da
Brent Gardner <bgardner@squarelabs.net>
2017-02-06 02:13:48 -0700

    Add nanogui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <signal.h>
 #include <fstream>
 
+#include <GL/glew.h>
+#include <nanogui/formhelper.h>
+#include <nanogui/nanogui.h>
+
 #include "VrInput.h"
 #include "SdlContext.h"
 #include "SdlTargetWindow.h"
@@ -12,6 +16,29 @@
 using namespace std;
 
 int main() {
+	FormHelper *gui = new FormHelper(screen);
+	ref<Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
+	gui->addGroup("Basic types");
+	gui->addVariable("bool", bvar);
+	gui->addVariable("string", strvar);
+
+	gui->addGroup("Validating fields");
+	gui->addVariable("int", ivar);
+	gui->addVariable("float", fvar);
+	gui->addVariable("double", dvar);
+
+	gui->addGroup("Complex types");
+	gui->addVariable("Enumeration", enumval, enabled)
+	->setItems({"Item 1", "Item 2", "Item 3"});
+	gui->addVariable("Color", colval);
+
+	gui->addGroup("Other widgets");
+	gui->addButton("A button", [](){ std::cout << "Button pressed." << std::endl; });
+
+	screen->setVisible(true);
+	screen->performLayout();
+	window->center();
+
     // Setup
 	NavigateCommand navigateCmd;
 	SdlContext sdl;
